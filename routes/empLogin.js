737db1f0355085d751ac06c1fec40cfc6a40c352
rhyrhy99 <71307480+rhyrhy99@users.noreply.github.com>
2021-01-06 16:34:38 -0600

    Update empLogin.js

--- routes/empLogin.js
+++ routes/empLogin.js
@@ -1,13 +1,36 @@
 const router = require("express").Router();
-var connection = require("../modules/connection.js");
+const { connection } = require("../modules/connection.js");
+const { validateEmailAddress } = require("../modules/validation.js");
 
 
-//login handle
+//employee login handle
 router.get('/empLogin',(req,res)=>{
-    var username = req.query.username;
-    var password = req.query.password;
+    var username = req.body.username;
+    var password = req.body.password;
 
-    if(username && password){
+    //check if username is entered
+    if (!username)
+    return res.status(404).render("error", {
+      code: "404",
+      message: "No username provided!",
+    });
+    
+    //check if password is entered
+    else if (!password)
+    return res.status(404).render("error", {
+      code: "404",
+      message: "No password provided!",
+    });
+    
+    //validate email address
+    else if (!validateEmailAddress(req.query.username))
+    return res.status(400).render("error", {
+      code: "400",
+      message: "Invalid email address!",
+    });
+
+    //if both values entered, check values in DB
+    else{
        connection.query("SELECT * FROM EMPLOYEE WHERE EMAIL = ? AND PASSWORD = ?", [username, password], function(error, results){
             if (results.length > 0) {
                 res.redirect('/home');
@@ -16,13 +39,9 @@ router.get('/empLogin',(req,res)=>{
         }			
         res.end();
     });
-    } else {
-    res.send("Please enter Username and Password!");
-    res.end();
-}
-       }
-       );
-       
+    } 
+});
+      
    
 
 module.exports = router;
