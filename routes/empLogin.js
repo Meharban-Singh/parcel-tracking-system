b767a363bfb4784a80d53baf2ae25ae48ab0cd8c
rhyrhy99 <71307480+rhyrhy99@users.noreply.github.com>
2021-01-07 16:44:29 -0600

    Update empLogin.js
    commenting and indenting

--- routes/empLogin.js
+++ routes/empLogin.js
@@ -4,41 +4,52 @@ const { validateEmailAddress } = require("../modules/validation.js");
 
 //employee login handle
 router.post("/empLogin", (req, res) => {
-  var username = req.body.username;
-  //req.body.username; //working on it, always returns null
 
+  //get values of username and password entered by user
+  var username = req.body.username;
   var password = req.body.password;
+
+  //testing to check values -- delete later
   console.log(username, password);
+
   //check if username is entered
   if (!username)
     return res.status(404).render("error", {
       code: "404",
       message: "No username provided!",
     });
+
   //check if password is entered
   else if (!password)
     return res.status(404).render("error", {
       code: "404",
       message: "No password provided!",
     });
+
   //validate email address
   else if (!validateEmailAddress(req.body.username))
     return res.status(400).render("error", {
       code: "400",
       message: "Invalid email address!",
     });
+
   //if both values entered, check values in DB
   else {
     connection.query(
       "SELECT * FROM EMPLOYEE WHERE EMAIL = ? AND PASSWORD = ?",
       [username, password],
       async function (error, results) {
-        if (!results) {
+        if(error){
+          console.error();
+        }
+        else if (!results) {
           res.send("No account exists for that username!");
-        } else {
+        } 
+        else {
           if (results.length > 0) {
             res.redirect("/home");
-          } else {
+          } 
+          else {
             res.status(401).render("error", {
               code: "401",
               message: "Email and password do not match",
