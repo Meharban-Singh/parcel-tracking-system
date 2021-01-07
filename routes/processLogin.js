593abac7ee455482715eb5fe3b7f1f3a1efff99d
rhyrhy99 <71307480+rhyrhy99@users.noreply.github.com>
2021-01-07 16:55:22 -0600

    Create processLogin.js
    chnaged route name from empLogin to processLogin

---
+++ routes/processLogin.js
@@ -0,0 +1,64 @@
+const router = require("express").Router();
+const { connection } = require("../modules/connection.js");
+const { validateEmailAddress } = require("../modules/validation.js");
+
+//employee login handle
+router.post("/processLogin", (req, res) => {
+
+  //get values of username and password entered by user
+  var username = req.body.username;
+  var password = req.body.password;
+  var stay_signed_in = req.body.stay_signed_in;
+
+  //testing to check values -- delete later
+  console.log(username, password, stay_signed_in);
+
+  //check if username is entered
+  if (!username)
+    return res.status(404).render("error", {
+      code: "404",
+      message: "No username provided!",
+    });
+
+  //check if password is entered
+  else if (!password)
+    return res.status(404).render("error", {
+      code: "404",
+      message: "No password provided!",
+    });
+
+  //validate email address
+  else if (!validateEmailAddress(req.body.username))
+    return res.status(400).render("error", {
+      code: "400",
+      message: "Invalid email address!",
+    });
+
+  //if both values entered, check values in DB
+  else {
+    connection.query(
+      "SELECT * FROM EMPLOYEE WHERE EMAIL = ? AND PASSWORD = ?",
+      [username, password],
+      async function (error, results) {
+        if(error){
+          console.error();
+        }
+        else if (!results) {
+          res.send("No account exists for that username!");
+        } 
+        else {
+          if (results.length > 0) {
+            res.redirect("/home");
+          } 
+          else {
+            res.status(401).render("error", {
+              code: "401",
+              message: "Email and password do not match",
+            });
+          }
+        }
+      }
+    );
+  }
+});
+module.exports = router;
