682207803f86bb0c0f16c15d10a00aa1fb928480
rhyrhy99 <71307480+rhyrhy99@users.noreply.github.com>
2021-01-07 17:21:22 -0600

    Update processLogin.js
    created local var for value of stay_signed in

--- routes/processLogin.js
+++ routes/processLogin.js
@@ -8,29 +8,29 @@ router.post("/processLogin", (req, res) => {
   //get values of username and password entered by user
   var username = req.body.username;
   var password = req.body.password;
-  var stay_signed_in = req.body.stay_signed_in;
+  var signIn = req.body.stay_signed_in;
 
   //testing to check values -- delete later
-  console.log(username, password, stay_signed_in);
+  console.log(username, password, signIn);
 
   //check if username is entered
   if (!username)
-    return res.status(404).render("error", {
+    return res.status(401).render("error", {
       code: "404",
       message: "No username provided!",
     });
 
   //check if password is entered
   else if (!password)
-    return res.status(404).render("error", {
-      code: "404",
+    return res.status(401).render("error", {
+      code: "401",
       message: "No password provided!",
     });
 
   //validate email address
   else if (!validateEmailAddress(req.body.username))
     return res.status(400).render("error", {
-      code: "400",
+      code: "401",
       message: "Invalid email address!",
     });
 
@@ -40,15 +40,12 @@ router.post("/processLogin", (req, res) => {
       "SELECT * FROM EMPLOYEE WHERE EMAIL = ? AND PASSWORD = ?",
       [username, password],
       async function (error, results) {
-        if(error){
-          console.error();
-        }
-        else if (!results) {
+        if (!results) {
           res.send("No account exists for that username!");
         } 
         else {
           if (results.length > 0) {
-            res.redirect("/home");
+            res.status(200).redirect("/home");
           } 
           else {
             res.status(401).render("error", {
