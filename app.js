cf2afcb992effda7bc880601202d3825a43365a8
Meharban Singh <meharbansingh10.ms@gmail.com>
2021-01-06 19:45:24 -0600

    Fixed the req.body undefined bug

--- app.js
+++ app.js
@@ -2,14 +2,12 @@ const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
- 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // parse application/json
-app.use(bodyParser.json())
-
-
+app.use(bodyParser.json());
 
 const exphbs = require("express-handlebars");
 
@@ -27,13 +25,13 @@ require("./modules/connection.js");
 
 // Middlewares
 app.use(cookieParser());
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // routes
 app.use(require("./routes/index"));
 app.use(require("./routes/loginpage"));
 app.use(require("./routes/empLogin"));
-app.use(require("./routes/home"))
+app.use(require("./routes/home"));
 app.use(require("./routes/admin"));
 app.use(require("./routes/track"));
 app.use(require("./routes/error")); // this MUST be the last middleware so that ir handles all 'other' routes
