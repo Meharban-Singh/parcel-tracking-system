eba19e51d2c739d2422259b9e43d4bcf9cfbb99d
Meharban Singh <meharbansingh10.ms@gmail.com>
2021-01-07 19:18:19 -0600

    Added login page with all functioanlity

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { connection } = require("../modules/connection.js");
 const { validateEmailAddress } = require("../modules/validation.js");
+const bcrypt = require("bcrypt"); // to hash the password
 
 //login handler
 router.get("/login", (req, res) => {
@@ -9,51 +10,61 @@ router.get("/login", (req, res) => {
 
 //employee login handle
 router.post("/processLogin", (req, res) => {
-  //get values of username and password entered by user
-  var username = req.body.username;
-  var password = req.body.password;
-  var signIn = req.body.stay_signed_in;
+  //get values of email and password entered by user
+  let email = req.body.email.toLowerCase();
+  let password = req.body.password;
+  let signIn = req.body.stay_signed_in;
 
-  //testing to check values -- delete later
-  console.log(username, password, signIn);
-
-  //check if username is entered
-  if (!username)
-    return res.status(401).render("error", {
-      code: "404",
-      message: "No username provided!",
+  //check if email is entered
+  if (!email) {
+    return res.status(400).render("login", {
+      error_message: "No email provided!",
     });
-  //check if password is entered
-  else if (!password)
-    return res.status(401).render("error", {
-      code: "401",
-      message: "No password provided!",
+  } else if (!password) {
+    //check if password is entered
+    return res.status(400).render("login", {
+      error_message: "No password provided!",
     });
-  //validate email address
-  else if (!validateEmailAddress(req.body.username))
-    return res.status(400).render("error", {
-      code: "401",
-      message: "Invalid email address!",
+  } else if (!validateEmailAddress(req.body.email)) {
+    //validate email address
+    return res.status(400).render("login", {
+      error_message: "Invalid email address format!",
     });
-  //if both values entered, check values in DB
-  else {
+  } else {
+    //if both values entered, check values in DB
     connection.query(
-      "SELECT * FROM employee WHERE EMAIL = ? AND PASSWORD = ?",
-      [username, password],
+      "SELECT * FROM employee WHERE email = ?",
+      [email],
       async function (error, results) {
         if (error) {
-          console.error();
-        } else if (!results) {
-          res.send("No account exists for that username!");
+          console.error(error);
+          return res.status(500).render("error", {
+            code: "500",
+            messsage: "Something went wrong.",
+          });
+        } else if (results.length === 0) {
+          res.status(200).render("login", {
+            error_message: "No account exists for that email!",
+          });
         } else {
-          if (results.length > 0) {
-            res.redirect("/home");
-          } else {
-            res.status(401).render("error", {
-              code: "401",
-              message: "Email and password do not match",
-            });
-          }
+          // No errors so far, need to check if password is correct.
+          bcrypt.compare(password, results[0].password, function (err, result) {
+            if (err) {
+              return res.status(500).render("error", {
+                code: "500",
+                message: "Something went wrong!",
+              });
+            }
+            if (result) {
+              // TODO: add redirection Correct!
+              res.status(202).redirect("/");
+            } else {
+              // password is incorrect
+              res.status(200).render("login", {
+                error_message: "Email and password do not match",
+              });
+            }
+          });
         }
       }
     );
