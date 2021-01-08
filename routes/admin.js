b44e5f375e700942f4892915043a951d4c32caaa
rhyrhy99 <71307480+rhyrhy99@users.noreply.github.com>
2021-01-08 01:38:41 -0600

    Update admin.js
    removed route to /home because file was deleted

--- routes/admin.js
+++ routes/admin.js
@@ -5,4 +5,8 @@ router.get('/admin',(req,res)=>{
     res.status(200).render("admin");
 })
 
+//employee homepage handler -- employee/parcels
+router.get("/employee/parcels",(req,res)=>{
+    res.status(200).render("employeeHome");
+})
 module.exports = router;
