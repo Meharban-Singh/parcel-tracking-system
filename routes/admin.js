7ddf26322468fb646e5d5c61e8c338175ff097e0
rhyrhy99 <71307480+rhyrhy99@users.noreply.github.com>
2021-01-08 10:47:12 -0600

    Update admin.js
    routes within admin account

--- routes/admin.js
+++ routes/admin.js
@@ -9,4 +9,20 @@ router.get('/admin',(req,res)=>{
 router.get("/employee/parcels",(req,res)=>{
     res.status(200).render("employeeHome");
 })
+
+//parcel list handler -- admin/parcels
+router.get("/admin/myParcels",(req,res)=>{
+    res.status(200).render("adminparcels");
+})
+
+//open registration page
+router.get("/admin/register",(req,res)=>{
+    res.status(200).render("register");
+})
+
+//open admin personal information
+router.get("/admin/employeeinfo",(req,res)=>{
+    res.status(200).render("admininfo");
+})
+
 module.exports = router;
