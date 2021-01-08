56fdc43889fa1c46e6d089827ab052157eda4eff
Meharban Singh <meharbansingh10.ms@gmail.com>
2021-01-08 17:28:38 -0600

    prepared a massive object which can be used to render things in handlebars later

--- routes/track.js
+++ routes/track.js
@@ -42,18 +42,95 @@ router.get("/parcel", (req, res) => {
 
   // Search for the entered parcel
   connection.query(
-    "SELECT * FROM parcel WHERE trackingNumber = ?",
+    "SELECT * FROM parcel INNER JOIN delivery WHERE trackingNumber = ?",
     [req.query.trackingNumber],
     async (error, results) => {
-      if (error || !results)
+      if (error) {
+        return res.status(500).render("error", {
+          message: "Something went wrong!",
+          code: "500",
+        });
+      } else if (results.length === 0) {
         return res.status(404).render("index", {
           numbers,
           error_message:
             "Tracking number not found! Please double-check the number. Contact support if error persists.",
         });
+      } else {
+        // all clear
+        // Preparation of conetect obect to be sent to handlebars
+        let contextObject = {};
+        contextObject.trackingNumber = results[0].trackingNumber;
+        contextObject.customerName = results[0].customerName;
+        contextObject.sourceLocation = results[0].sourceLocation;
+        contextObject.destinationLocation = results[0].destinationLocation;
 
-      console.log(results);
-      res.status(200).render("parcelDetails", {});
+        /*
+        Create an array of the form:[
+          {
+            date:,
+            details: [
+              {time, location, status}
+            ]
+          },
+          ...
+        ]
+        */
+        let deliveries = [];
+        for (let result of results) {
+          // Add each delivery detail to a detailsObject
+          let detailsObject = {};
+          detailsObject.currentCity = result.currentCity;
+          detailsObject.currentProvince = result.currentProvince;
+          detailsObject.status = result.status;
+
+          // Time conversion
+          let hours = result.date.getHours();
+          let mins = result.date.getMinutes() + 1;
+          let period = "AM";
+
+          if (hours > 11) {
+            period = "PM";
+            hours = hours % 12;
+
+            if (hours == 0) hours = 12;
+          }
+
+          detailsObject.time = `${hours}:${mins} ${period}`;
+
+          // Check if the current date is already in the deliveries array
+          let date = result.date
+            .toUTCString() // convert date to Fri, Jan 08, 2021 24:59:59 GMT
+            .split(" ") // change that to an array ["Fri,", "Jan", "08", "2021" ..]
+            .splice(0, 3) // get the first 3 elements of the array
+            .join(" "); // Join them with spaces to get: Fri, Jan 08
+          let added = false;
+          for (let delivery of deliveries) {
+            if (delivery.date == date) {
+              // If date is already in the list, then just push the details there.
+              delivery.details.push(detailsObject);
+              added = true; // set added to false
+              break;
+            }
+          }
+
+          // If not added yet, that means date is not found, so create new obj in deliveries
+          if (!added) {
+            // Push that object to the deliveries array
+            let tempObject = {};
+            tempObject.date = date;
+            tempObject.details = [detailsObject];
+
+            deliveries.push(tempObject);
+          }
+        }
+
+        //cAdd deliveries to the context object
+        contextObject.deliveries = deliveries;
+
+        console.log(contextObject);
+        res.status(200).render("parcelDetails", contextObject);
+      }
     }
   );
 });
