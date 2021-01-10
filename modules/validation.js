5bd6bb6fb1fc57d4f7210a50af92c189ca68840e
rhyrhy99 <71307480+rhyrhy99@users.noreply.github.com>
2021-01-10 01:42:33 -0600

    Update validation.js
    changed validate password to return only boolean

--- modules/validation.js
+++ modules/validation.js
@@ -1,3 +1,5 @@
+const e = require("express");
+
 const PROVINCES = ["AB", "BC", "MB", "NB", "NL", "NS", "ON", "PE", "QC", "SK"];
 
 /**
@@ -56,61 +58,56 @@ function validateEmailAddress(email) {
  * @returns {boolean|string} True if valid, reason if invalid
  */
 function validatePassword(password) {
-  // If no parameter provided
-  if (!password) return false;
-
   //input prep
   password = password.trim();
 
+  // If no parameter provided
+  if (!password) {
+    return false;
+  }
   // Length check
-  if (password.length < 8 || password.length > 16)
-    return "Password length should be between 8 and 16";
-
-  // Metrics =>
-  let hasNumber = false,
+  else if (password.length < 8 || password.length > 16){
+    return false;
+  }
+  else{
+    // Metrics =>
+    let hasNumber = false,
     hasUpperCase = false,
     hasLowerCase = false,
     hasSymbol = false;
 
-  // Loop through password
-  for (let i = 0; i < password.length; i++) {
-    let char = password.charAt(i);
+    // Loop through password
+    for (let i = 0; i < password.length; i++) {
+      let char = password.charAt(i);
 
-    // If one number is found, set hasNumber to true
-    if (char >= "0" && char <= "9") {
-      hasNumber = true;
-      continue;
-    }
+      // If one number is found, set hasNumber to true
+      if (char >= "0" && char <= "9") {
+        hasNumber = true;
+        continue;
+      }
 
-    // If one symbol is found, set hasSymbol to true
-    if ("`~!@#$%^&*()_-+[{]}|\\;:<>,./?".split("").includes(char)) {
-      hasSymbol = true;
-      continue;
-    }
+      // If one symbol is found, set hasSymbol to true
+      if ("`~!@#$%^&*()_-+[{]}|\\;:<>,./?".split("").includes(char)) {
+        hasSymbol = true;
+        continue;
+      }
 
-    // If one uppercase char is found, set hasUpperCase to true
-    if (char === char.toUpperCase()) {
-      hasUpperCase = true;
-      continue;
-    }
+      // If one uppercase char is found, set hasUpperCase to true
+      if (char === char.toUpperCase()) {
+        hasUpperCase = true;
+        continue;
+      }
 
-    // If one lowercase char is found, set hasLowerCase to true
-    if (char === char.toLowerCase()) {
-      hasLowerCase = true;
-      continue;
+      // If one lowercase char is found, set hasLowerCase to true
+      if (char === char.toLowerCase()) {
+        hasLowerCase = true;
+        continue;
+      }
     }
+    // All else is true
+    if(hasLowerCase && hasNumber && hasSymbol && hasUpperCase) return true;
+    else return false;
   }
-
-  // Return corresponding messages
-  if (!hasNumber) return "Password should have at least one digit";
-  if (!hasUpperCase)
-    return "Password should have at least one uppercase letter";
-  if (!hasLowerCase)
-    return "Password should have at least one lowercase letter";
-  if (!hasSymbol) return "Password should have at least one symbol";
-
-  // All else is true
-  return true;
 }
 
 /**
