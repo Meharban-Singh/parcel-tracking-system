6db3c90908e67f00fd269470d93fd97b800b636d
rhyrhy99 <71307480+rhyrhy99@users.noreply.github.com>
2021-01-10 01:07:59 -0600

    Update admin.js
    /process registration

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,8 @@
 const router = require("express").Router();
+const { connection } = require("../modules/connection.js");
+const bcrypt = require("bcrypt"); // to hash the password
+const { validatePassword, validateEmailAddress } = require("../modules/validation.js");
+const SALT_ROUNDS = 10;
 
 //admin handler
 router.get('/admin',(req,res)=>{
@@ -20,9 +24,107 @@ router.get("/admin/employeeinfo",(req,res)=>{
     res.status(200).render("admininfo");
 })
 
-//open admin personal information
+//add parcel form
 router.get("/admin/addparcel",(req,res)=>{
     res.status(200).render("addParcel");
+})
+
+//register employee
+router.post("/admin/processRegistration", (req,res)=>{
+    //get values entered by user
+    let fname = req.body.fname;
+    let lname = req.body.lname;
+    let email = req.body.email;
+
+    //check if user entered first name
+    if(!fname){
+        return res.status(400).render("register", {
+            error_message: "No first name provided!",
+        });
+    }
+
+    //check if user entered last name
+    else if(!lname){
+        return res.status(400).render("register", {
+            error_message: "No last name provided!"
+        });
+    }
+
+    //check if user entered email address
+    else if(!email){
+        return res.status(400).render("register",{
+            error_message: "No email address provided!"
+        });
+    }
+
+    //check is email is valid
+    else if(!validateEmailAddress(email)){
+        returnres.status(400).render("register",{
+            error_message: "Invalid email address."
+        })
+    }
+    //check if password is entered
+    else if(!req.body.password){
+        return res.status(400).render("register",{
+            error_message: "No password provided!"
+        });
+    }
+    
+    //check if password is valid
+    else if(!validatePassword(req.body.password)){
+        return res.status(400).render("register",{
+            error_message: "Password must be 8-16 characters long, and have at least one Uppercase, one lowercase, one symbol and one digit."
+        })
+    }
+
+    else{
+        //check id account with given email already exists
+        connection.query(
+            "SELECT * FROM employee WHERE email = ?",
+            [email],
+            async function (error, results) {
+                if(error){
+                    console.log(error);
+                    return res.status(500).render("register",{
+                        code: "500",
+                        message: "Something went wrong, please try again!"
+                    })
+                }
+                else if(results.length>0){
+                    res.status(400).render("register",{
+                        error_message: "An account with this email address already exists."
+                    })
+                }
+                else{
+                    bcrypt.hash(req.body.password,SALT_ROUNDS, function(err,hash){
+                        if(err){
+                            res.status(400).render("register",{
+                                error_message: "An error occured, please try again!"
+                            })
+                        }else{
+                            let password=hash;
+                        }
+                    })
+                    connection.query(
+                        "INSERT INTO employee (fname, lname, email, password) VALUES (?, ?, ?, ?)",
+                        [fname, lname, email, password],
+                        async function(errors, result){
+                            if(errors){
+                                res.status(400).render("register",{
+                                    error_message: "Processing error, please try again!"
+                                })
+                            }
+                            else{
+                                res.status(200).render("register",{
+                                    message: "Registration successful!"
+                                })
+                            }
+                        }
+                    )
+                }
+        })
+    }
+   
 })
 
 module.exports = router;
